026b139b3f98300e8a78f5c287259c93603d93cb
george <jorspunk@gmail.com>
2023-06-12 20:40:57 +0700

    first comitt

---
+++ hello.js
@@ -0,0 +1,29 @@
+var a = 2;
+var b = 4
+
+if(a+b)
+{
+    console.log(a+b);
+}
+else if(a+b<7)
+{
+    consol.log(a+b);
+}
+else 
+{
+    console.log(a+b);
+}
+
+
+
+let fruits =['apple','banana','orange']
+
+for(let i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i]);
+}
+
+for(let i=fruits.length-1;i>=0;i--)
+{
+    console.log(fruits[i]);
+}
